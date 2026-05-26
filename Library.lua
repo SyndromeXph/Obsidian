44ca6b052b08189b6ec4e612db15da66fd9c3aad
Yuxingchen <160198513+SyndromeXph@users.noreply.github.com>
2026-05-27 02:59:25 +0800

    Add files via upload

--- Library.lua
+++ Library.lua
@@ -230,20 +230,20 @@ local Library = {
 
     IsLightTheme = false,
     Scheme = {
-        BackgroundColor = Color3.fromRGB(15, 15, 15),
-        MainColor = Color3.fromRGB(25, 25, 25),
-        AccentColor = Color3.fromRGB(125, 85, 255),
-        OutlineColor = Color3.fromRGB(40, 40, 40),
-        FontColor = Color3.new(1, 1, 1),
-        Font = Font.fromEnum(Enum.Font.Code),
+        BackgroundColor = Color3.fromRGB(10, 10, 10),     
+MainColor = Color3.fromRGB(20, 20, 20),           
+AccentColor = Color3.fromRGB(255, 50, 50),        
+OutlineColor = Color3.fromRGB(50, 50, 50),        
+FontColor = Color3.new(1, 1, 1),                 
+Font = Font.fromEnum(Enum.Font.Code),
 
-        RedColor = Color3.fromRGB(255, 50, 50),
-        DestructiveColor = Color3.fromRGB(220, 38, 38),
-        DarkColor = Color3.new(0, 0, 0),
-        WhiteColor = Color3.new(1, 1, 1),
+RedColor = Color3.fromRGB(255, 50, 50),           
+DestructiveColor = Color3.fromRGB(220, 38, 38),    
+DarkColor = Color3.new(0, 0, 0),                  
+WhiteColor = Color3.new(1, 1, 1),                 
 
-        BackgroundImageEnabled = false,
-        BackgroundImage = "",
+        BackgroundImageEnabled = true,
+        BackgroundImage = "https://raw.githubusercontent.com/SyndromeXph/icon/refs/heads/main/e83e251630ab0908949fc6fdcb29b029.png",
         WindowGlow = true,
     },
 
